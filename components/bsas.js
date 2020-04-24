65f035874e3321d976526c556ac9e31f781c2646
jpgallegos <juanpablo.gallegos@followoz.com>
2020-04-24 19:40:38 -0300

    changed folders structure and added bsas and rosario components

---
+++ components/bsas.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import Card from "../components/ui/card";
+import { Box, Grid, Heading } from "@chakra-ui/core";
+
+const Bsas = () => {
+  return (
+    <Box padding={3}>
+      <Heading as="h2" textAlign="center" mb={3}>Buenos Aires</Heading>
+      <Grid
+        templateColumns="repeat(auto-fit, 350px)"
+        gap={2}
+        justifyContent="center"
+      >
+        <Card price={0} />
+        <Card />
+        <Card shipment={false} />
+        <Card area="Villa crespo" />
+        <Card area="Palermo" />
+      </Grid>
+    </Box>
+  );
+};
+
+export default Bsas;
--- pages/barbijos.js
+++ pages/barbijos.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import Layout from '../components/layout';
-import Card from '../components/ui/card';
-import { Box, Grid } from '@chakra-ui/core';
+import React from "react";
+import Layout from "../components/layout/layout";
+import Rosario from '../components/rosario';
+import Bsas from '../components/bsas';
+
+import { Box, Grid } from "@chakra-ui/core";
 
 const Barbijos = () => (
   <Layout>
-    <Box padding={3}>
-      <Grid
-        templateColumns="repeat(auto-fit, 350px)"
-        gap={2}
-        justifyContent="center"
-      >
-        <Card price={0} />
-        <Card />
-        <Card shipment={false} />
-        <Card area="Villa crespo" />
-        <Card area="Palermo" />
-      </Grid>
-    </Box>
+      <Rosario />
+      <Bsas />
   </Layout>
 );
 
