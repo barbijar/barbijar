85bf2c63bfdfb366c7004bf2a802ad6f85a4b6d4
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-04-27 03:14:04 -0300

    fetch data in the main componente to search masks

--- pages/barbijos.js
+++ pages/barbijos.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layout/layout";
-import Rosario from '../components/rosario';
-import Bsas from '../components/bsas';
+import Rosario from "../components/rosario";
+import Bsas from "../components/bsas";
+import { CommerceContext } from "../context/CommerceContext";
+import useCommerces from "../hooks/useCommerces";
 
-import { Box, Grid } from "@chakra-ui/core";
+import axios from "axios";
+import { Box, Grid, PseudoBox } from "@chakra-ui/core";
 
-const Barbijos = () => (
-  <Layout>
-      <Rosario />
-      <Bsas />
-  </Layout>
-);
+const Barbijos = () => {
+  const [commerces, setCommerces] = useState([]);
+  const [error, setError] = useState(null);
+
+  const { Commerces } = useCommerces(commerces);
+
+  useEffect(() => {
+    const getCommerces = async () => {
+      try {
+        const res = await axios.get(
+          "https://calm-brook-73441.herokuapp.com/commerce"
+        );
+        setCommerces(res.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    getCommerces();
+  }, []);
+
+  return (
+    <Layout>
+      <>
+        <PseudoBox as="div">
+          <Commerces />
+        </PseudoBox>
+      </>
+    </Layout>
+  );
+};
 
 export default Barbijos;
