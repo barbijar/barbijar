9d2d6fda7d9892c92c5855c9543a4b0a470b222b
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-04-28 06:27:42 -0300

    added spinner

--- pages/barbijos.js
+++ pages/barbijos.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "../components/layout/layout";
 import useCommerces from "../hooks/useCommerces";
 import useFilter from "../hooks/useFilter";
+import Loading from "../components/ui/loading";
 
 import axios from "axios";
 import { Box, Grid, PseudoBox } from "@chakra-ui/core";
@@ -10,14 +11,17 @@ const Barbijos = () => {
   const [commerces, setCommerces] = useState([]);
   const [error, setError] = useState(null);
   const [filteredProvince, setFilteredProvince] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { Commerces } = useCommerces(filteredProvince);
-  const { FilterByProvince, prov} = useFilter(commerces);
+  const { FilterByProvince, prov } = useFilter(commerces);
 
   useEffect(() => {
     const getCommerces = async () => {
       if (prov) {
-        const filterProv = commerces.filter(commerce => commerce.province.id == prov);
+        const filterProv = commerces.filter(
+          (commerce) => commerce.province.id == prov
+        );
 
         setFilteredProvince(filterProv);
       } else {
@@ -25,8 +29,14 @@ const Barbijos = () => {
           const res = await axios.get(
             "https://calm-brook-73441.herokuapp.com/commerce"
           );
-          setCommerces(res.data);
-          setFilteredProvince(res.data);
+
+          setIsLoading(true);
+
+          setTimeout(() => {
+            setIsLoading(false);
+            setCommerces(res.data);
+            setFilteredProvince(res.data);
+          }, 3000);
         } catch (error) {
           setError(error);
         }
@@ -35,14 +45,15 @@ const Barbijos = () => {
     getCommerces();
   }, [prov]);
 
+  const showCommerces = isLoading ? <Loading /> : <Commerces />;
+
   return (
     <Layout>
-      <>
-        <PseudoBox as="div">
-          <FilterByProvince />
-          <Commerces />
-        </PseudoBox>
-      </>
+      <PseudoBox as="div">
+        <FilterByProvince />
+
+        {showCommerces}
+      </PseudoBox>
     </Layout>
   );
 };
