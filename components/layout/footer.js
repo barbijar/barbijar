0ca30395ea4520022a0e4bb8a3c74f55790fd80e
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-05-03 02:26:12 -0300

    new title for footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import {
-  Flex,
-  Text
-} from '@chakra-ui/core';
+import React from "react";
+import { Flex, Text, PseudoBox } from "@chakra-ui/core";
 
 const Footer = () => {
-    const fecha = new Date();
-    const anio = fecha.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
 
   return (
     <>
-        <Flex 
+      <Flex
         justifyContent="center"
         alignItems="center"
         wrap="wrap"
         p="1.5rem"
         bg="blue.700"
-        color="blue.50">
-            <Text >
-            Barbijar, Copyright © {anio}
-            </Text>
-        </Flex>
+        color="blue.50"
+      >
+        <Text>
+          Hecho con &hearts; por el equipo de BarbijAr © {year}
+        </Text>
+      </Flex>
     </>
   );
 };
