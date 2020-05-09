195a33416fb754f3fb05cd2628d4a7e429ebb0fe
monazo22 <federico_zuccala@hotmail.com>
2020-05-09 18:13:32 -0300

    removed the landing component of salud.js and emprender.js

--- pages/emprender.js
+++ pages/emprender.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Layout from "../components/layout/layout";
-import Landing from "../components/ui/landing";
-import { MdContentCut } from "react-icons/md";
 
 const Emprender = () => (
   <Layout>
-    <Landing title="Emprender" desc="Descripción Emprender" icon={MdContentCut} href="/emprender" hrefValue="Emprender" />
+    
   </Layout>
 );
 
--- pages/salud.js
+++ pages/salud.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Layout from "../components/layout/layout";
-import Landing from "../components/ui/landing";
-import {MdAddBox} from 'react-icons/md';
 const Salud = () => (
   <Layout>
-    <Landing title="Salud" desc="Descripción Salud" icon={MdAddBox} href="/salud" hrefValue="salud" />
+    
   </Layout>
 );
 
