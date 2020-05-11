01aa637d0313fb88a7af543667b4947befc16af1
Juan Pablo Gallegos <jpgallegos20@gmail.com>
2020-05-11 08:04:12 -0300

    it will display if it is on sm or xl size

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { Flex, Text, PseudoBox } from "@chakra-ui/core";
+import { Flex, Text, Box } from "@chakra-ui/core";
 
 const Footer = () => {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <>
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        wrap="wrap"
-        p="1.5rem"
-        bg="blue.700"
-        color="blue.50"
-      >
-        <Text>
-          Hecho con &hearts; por el equipo de BarbijAr © {year}
-        </Text>
+    <Box
+      position="absolute"
+      bottom="0"
+      width="100%"
+      display={{ sm: "none", md: "none", lg: "block" }}
+    >
+      <Flex justifyContent="center" p="1.5rem" bg="blue.700" color="blue.50">
+        <Text>Hecho con &hearts; por el equipo de BarbijAr © {year}</Text>
       </Flex>
-    </>
+    </Box>
   );
 };
 
